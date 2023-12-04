d49fbb82e367d7acc0e79f5409b627b5a13de879
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-04 18:11:19 +0300

    use methods from Math module in Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,8 @@ import Math;
 
 int main() 
 {
-	std::cout << "Best Number is: " << Add(10, 13);
+	std::cout << "Best Number is: " << Add(10, 13) << std::endl;
+	std::cout << "25 - 2: " << Subtract(25, 2);
 
 	return 0;
 }
