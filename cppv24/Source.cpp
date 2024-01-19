bd5cdc6f0becdab366f27ddba18ba9b8336fb575
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-19 17:30:38 +0300

    other variable types

--- cppv24/Source.cpp
+++ cppv24/Source.cpp
@@ -3,14 +3,34 @@ using namespace std;
 
 int main()
 {
-	string msg = "Hello Anil";
-	int age = 24;
-	float impLevel = 23.9;
+	string msg = "hello world";
+	int year = 2024;
+	float mySanity = 23.24;
 
-	cout << msg << endl;;
-	cout << msg << "\n";
-	cout << msg << endl;
-	cout << msg << endl;
-	cout << msg << "\n";
-	cout << "sooo im " << age << " years old" << endl;
+	char favChar = 'A';
+	char normalChar = 'f';
+
+	bool isDreamComeThrough = true;
+	bool isThinkingAboutStop = false;
+
+	cout << favChar << endl;
+	cout << normalChar << endl;
+
+	cout << isDreamComeThrough << endl;
+	// 1 (true)
+	cout << isThinkingAboutStop << endl;
+	// 0 (false)
+
+	// 1 tb is 1024 gb
+	// 1 gb is 1024 mb
+	// 1 mb is 1024 kb
+	// 1 kb is 1024 bytes
+	// 1 byte is 8 bits
+	// 1 bit is 1 bit that is either 0 or 1
+	// like 10101010110000110010101
+
+	cout << "the size of int is: " << sizeof(int) << " bytes" << endl;
+	// the size of int is: 4 bytes
+	cout << "the size of float is " << sizeof(float) << " bytes" << endl;
+	// the size of float is: 4 bytes
 }
