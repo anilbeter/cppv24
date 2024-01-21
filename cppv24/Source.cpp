29849ade478eef4398f4a3733990aed9beb2fb73
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-21 08:28:35 +0300

    fancier for loop

--- cppv24/Source.cpp
+++ cppv24/Source.cpp
@@ -5,16 +5,11 @@ using namespace std;
 
 int main()
 {
-	//for
-	int nums[] = { 1,2,3,4,5 };
-	for (int i = 0; i < sizeof(nums) / sizeof(int); i++)
+	// fancier for loop (look like while)
+	int i = 0;
+	for (; i < 10;)
 	{
-		cout << nums[i] << endl;
-	}
-
-	//foreach
-	for (int n : nums)
-	{
-		cout << n << endl;
+		i++;
+		cout << i << endl;
 	}
 }
