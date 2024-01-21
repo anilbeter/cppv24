3d3bd2d5afb386d64fe49f5de73d55756322eb4c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-21 09:42:03 +0300

    miles to km exercise

--- cppv24/Source.cpp
+++ cppv24/Source.cpp
@@ -1,64 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <limits>
 using namespace std;
 
 int main()
 {
-	// enums are basically named integers
-	enum class Mood
+	// converts miles to kilometers
+	// take input from the user, ask a distance in Miles
+	// convert that distance to kilometers and cout the value
+
+	const float CONVERSION = 1.609344;
+	float defaultMileValue = 0;
+
+	cout << "Hello! Welcome to Miles to Kilometers converter" << endl;
+	cout << "Please write distance in miles." << endl;
+
+	cin >> defaultMileValue;
+	while (!cin.good())
 	{
-		Happy, // defaults to 0
-		Angry, // previous + 1
-		Sad = 23, // also I can change the int manually
-		Bored // 24
-	};
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-	// create a variable of the enum type Mood, and give it a value of Happy
-	Mood mood = Mood::Happy;
-
-	int moodInt = (int)mood;
-
-	Mood sadly = Mood::Sad;
-	int favNum = (int)sadly;
-	cout << "My favorite num is " << favNum << endl;
-	// My favorite num is 23
-
-
-	switch (mood)
-	{
-	case Mood::Happy:
-		cout << "Happy happy happyy!" << endl;
-		break;
-	case Mood::Angry:
-		cout << "I DONT WANT TO SEE YOU!" << endl;
-		break;
-	case Mood::Sad:
-		cout << "Please dont leave" << endl;
-		break;
-	default:
-		cout << "Eh regular" << endl;
-		break;
+		cout << "Error input! Please give a number: \n>>>";
+		cin >> defaultMileValue;
 	}
 
+	float distanceInKilometers = defaultMileValue * CONVERSION;
 
-	switch (23)
-	{
-	case (int)Mood::Happy:
-		cout << "Happy happy happyy!" << endl;
-		break;
-	case (int)Mood::Angry:
-		cout << "I DONT WANT TO SEE YOU!" << endl;
-		break;
-	case (int)Mood::Sad:
-		cout << "Please dont leave" << endl;
-		break;
-	default:
-		cout << "Eh regular" << endl;
-		break;
-	}
-	// switch(0) -> Happy
-	// switch(1) -> Angry
-	// switch(23) -> Sad
-	// switch(3,4,5,...) -> "Eh regular"
+	cout << defaultMileValue << " mile equals to " << distanceInKilometers << " kilometers." << endl;
 }
