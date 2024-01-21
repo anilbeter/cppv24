264615f934975e77ce0ed4fdeee51ea818bbe718
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-21 13:43:02 +0300

    get words one by one without comma, period etc. and make them lowercase

--- cppv24/Source.cpp
+++ cppv24/Source.cpp
@@ -17,23 +17,32 @@ Driven by excitement, Lily embarked on a magical journey through the enchanted w
 After a day of exploration, Lily reached the Crystal Grove, a mesmerizing place adorned with crystals of every hue.She picked a small, shimmering crystal as a keepsake, and a warm glow surrounded her, granting her the ability to spread joy and wonder.
 
 Returning to Willowbrook, Lily shared her enchanting adventure, gifting crystals to friends and neighbors.The town transformed, infused with a magical energy.Lily's simple story became a legend, inspiring others to embrace the magic in everyday life, ensuring the enchantment of the Crystal Grove lived on in their hearts.)""";
-	
+
 	stringstream ss;
 	ss << text;
 	string nextStr = "";
 
-	// tüm stringleri(kelimeleri) tek tek yazdýrma:
 	while (ss >> nextStr)
 	{
-		cout << nextStr << endl;
-		/*
-		* TODO1)
-		* Bazý outputlar:
-		* forest,
-		* happen.Lily's
-		* gibi noktalama iþaretleri kelime yapýsýný bozuyor, bunlarý düzeltmem gerek
-		* TODO2)
-		* the ve The gibi büyük-küçük harf farklarý var, bunu düzeltmem gerek
-		*/
+		string word = "";
+		int lastPullIndex = 0;
+		for (char c : nextStr)
+		{
+			++lastPullIndex;
+			if (isalpha(c))
+			{
+				word.push_back(tolower(c));
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (lastPullIndex != nextStr.size() - 1)
+		{
+			string leftOver = nextStr.substr(lastPullIndex);
+			ss << ' ' << leftOver;
+		}
+		cout << word << endl;
 	}
 }
