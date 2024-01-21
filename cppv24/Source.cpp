fbccc9759978fbe9d6664ff7e3c506c98435dfd5
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-21 08:45:31 +0300

    switch

--- cppv24/Source.cpp
+++ cppv24/Source.cpp
@@ -5,10 +5,20 @@ using namespace std;
 
 int main()
 {
-	int i = 10;
-	do {
-		cout << i << endl;
-		i++;
-	} while (i < 10);
-	// do while will do the code in {} once first before checking the condition
+	int age = 24;
+	switch (age)
+	{
+	case 15:
+		cout << "Beginning of the chapter" << endl;
+		break;
+	case 18:
+		cout << "What the hell??" << endl;
+		break;
+	case 23:
+		cout << "Dream on" << endl;
+		break;
+	default:
+		cout << "NEVER BACKDWN" << endl;
+		break;
+	}
 }
