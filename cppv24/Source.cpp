6feccd7b043ef9d7cc8d1d3b01fd5d81985489d7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-21 08:46:07 +0300

    chaining switch cases

--- cppv24/Source.cpp
+++ cppv24/Source.cpp
@@ -5,15 +5,11 @@ using namespace std;
 
 int main()
 {
-	int age = 24;
+	int age = 23;
 	switch (age)
 	{
 	case 15:
-		cout << "Beginning of the chapter" << endl;
-		break;
 	case 18:
-		cout << "What the hell??" << endl;
-		break;
 	case 23:
 		cout << "Dream on" << endl;
 		break;
