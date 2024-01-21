3cff76e9169c4f639b6a8fbdaa2da7d39f2219af
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-21 09:08:41 +0300

    enums & switch use case

--- cppv24/Source.cpp
+++ cppv24/Source.cpp
@@ -5,16 +5,61 @@ using namespace std;
 
 int main()
 {
-	int age = 23;
-	switch (age)
+	// enums are basically named integers
+	enum Mood
 	{
-	case 15:
-	case 18:
-	case 23:
-		cout << "Dream on" << endl;
+		Happy, // defaults to 0
+		Angry, // previous + 1
+		Sad = 23, // also I can change the int manually
+		Bored // 24
+	};
+
+	// create a variable of the enum type Mood, and give it a value of Happy
+	Mood mood = Happy;
+
+	int moodInt = mood;
+	cout << mood << endl; // 0
+
+	Mood sadly = Sad;
+	int favNum = sadly;
+	cout << "My favorite num is " << favNum << endl;
+	// My favorite num is 23
+
+
+	switch (mood)
+	{
+	case Happy:
+		cout << "Happy happy happyy!" << endl;
+		break;
+	case Angry:
+		cout << "I DONT WANT TO SEE YOU!" << endl;
+		break;
+	case Sad:
+		cout << "Please dont leave" << endl;
 		break;
 	default:
-		cout << "NEVER BACKDWN" << endl;
+		cout << "Eh regular" << endl;
 		break;
 	}
+
+
+	switch (23)
+	{
+	case Happy:
+		cout << "Happy happy happyy!" << endl;
+		break;
+	case Angry:
+		cout << "I DONT WANT TO SEE YOU!" << endl;
+		break;
+	case Sad:
+		cout << "Please dont leave" << endl;
+		break;
+	default:
+		cout << "Eh regular" << endl;
+		break;
+	}
+	// switch(0) -> Happy
+	// switch(1) -> Angry
+	// switch(23) -> Sad
+	// switch(3,4,5,...) -> "Eh regular"
 }
