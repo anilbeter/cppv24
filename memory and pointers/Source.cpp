b48800a8ac7b4914caf77b51cb098cb7d70ad10e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-24 13:39:48 +0300

    reference

--- memory and pointers/Source.cpp
+++ memory and pointers/Source.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main()
 {
 	int x = 5;
+	// reference
 	cout << &x << endl; // 000000DB1B7DF5A4 (place that in memory)
+
+	int& anotherX = x;
+	cout << anotherX << endl; // 5
+	cout << &anotherX << endl; // 000000DB1B7DF5A4
+
+	anotherX = 23;
+	cout << x << endl; // 23
 }
