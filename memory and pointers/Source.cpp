78ad8a5a25fa370c3841a13cf652433fdd4ca973
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-24 14:04:30 +0300

    pointers

--- memory and pointers/Source.cpp
+++ memory and pointers/Source.cpp
@@ -4,13 +4,23 @@ using namespace std;
 int main()
 {
 	int x = 5;
-	// reference
-	cout << &x << endl; // 000000DB1B7DF5A4 (place that in memory)
 
-	int& anotherX = x;
-	cout << anotherX << endl; // 5
-	cout << &anotherX << endl; // 000000DB1B7DF5A4
+	// pointer. store the address of where x is
+	int* xPtr = &x; 
+	void* xPtr2 = &x; // a pointer stores and only stores a memory address. don't hold data about the type it points to
 
-	anotherX = 23;
+	// long story short, data type not a big deal in pointers
+	float floatNum = 9;
+	float* myFloatPointer = &floatNum;
+	cout << myFloatPointer << endl; // 000000F7A5F5F614
+	cout << *myFloatPointer << endl; // 9
+
+	cout << xPtr << endl; // 000000B089CFFC04
+	cout << xPtr2 << endl; // 000000B089CFFC04
+
+	*xPtr = 23; // dereferencing. access the value in the memory address
+	
+	cout << xPtr << endl; // 000000B089CFFC04
 	cout << x << endl; // 23
+	cout << *xPtr << endl; // 23
 }
