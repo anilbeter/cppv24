ac9ddc1a3a11e9b53590973654eaed7cfc395972
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-25 12:27:41 +0300

    arrays & pointers relation

--- memory and pointers/Source.cpp
+++ memory and pointers/Source.cpp
@@ -3,26 +3,13 @@ using namespace std;
 
 int main()
 {
-	int x = 5;
-
-	// pointer. store the address of where x is
-	int* xPtr = &x; 
-	void* xPtr2 = &x; // a pointer stores and only stores a memory address. don't hold data about the type it points to
-
-	// long story short, data type not a big deal in pointers
-	float floatNum = 9;
-	float* myFloatPointer = &floatNum;
-	cout << myFloatPointer << endl; // 000000F7A5F5F614
-	cout << *myFloatPointer << endl; // 9
-
-	cout << xPtr << endl; // 000000B089CFFC04
-	cout << xPtr2 << endl; // 000000B089CFFC04
-
-	*xPtr = 23; // dereferencing. access the value in the memory address
-	
-	cout << xPtr << endl; // 000000B089CFFC04
-	cout << x << endl; // 23
-	cout << *xPtr << endl; // 23
-
-	cout << *static_cast<int*>(xPtr2) << endl; // 23
+	int intArray[5] = { 1,2,3,4,5 };
+	cout << intArray << endl; // 000000ED8B52F5D8
+	cout << *intArray << endl; // 1 (first one in the array)
+	cout << *(intArray)+1 << endl; // 2
+	cout << *(intArray)+3 << endl; // 4
 }
+
+
+
+
