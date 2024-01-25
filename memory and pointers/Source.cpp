aeabce0a3f41ac6cfc79b264c98a7346834a9420
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-25 13:14:06 +0300

    heap memory

--- memory and pointers/Source.cpp
+++ memory and pointers/Source.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void printInt(int num)
+{
+	cout << num << endl;
+	{
+		int c = num;
+		cout << c << endl;
+	}
+
+	int d = 23;
+}
+
 int main()
 {
-	int intArray[5] = { 1,2,23,4,5 };
-	for (int i = 0; i < 5; i++)
-	{
-		cout << *(intArray + i) << endl;
-	}
-	cout << *(intArray + 2) << endl; // 23
+	int x = 5; // x is created on stack memory
+	int* heapX = new int{ 5 }; // an int variable with value 5 created on the heap. it's addres is stored in the stack variable heapX(which is a pointer). 
+	delete heapX;
+
+	printInt(x);
+
+	// heap can be used when need big storage.
+	// the memory allocated is not automatically removed unlike stack.
+	// to manually free the memory, you need to call delete on the pointer
 }
 
 
