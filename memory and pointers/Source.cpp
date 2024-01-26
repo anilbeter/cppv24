8192fb31812bcdef02cda2ff1b2f3f8ab74541bb
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-26 15:31:04 +0300

    shared pointer

--- memory and pointers/Source.cpp
+++ memory and pointers/Source.cpp
@@ -4,20 +4,26 @@ using namespace std;
 
 void printInt(int num)
 {
+	shared_ptr<int> intSharedPointer = make_shared<int>(5);
 
+	// shared pointer does allow copy. the raw memory will be free, when and only when the last shared pointer that holds it is deleted
+	shared_ptr<int> anotherShaderPointer = intSharedPointer;
+
+	// if you want to manually free the memory, do reset
+	anotherShaderPointer.reset();
+
+	// use unique pointer whenever possible,
+	// in real practice though, shared_pointer is used a lot
+	// a texture, any assets that is supposed to be shared between different objects. shard is good choice.
+	// unique ptr is more suited for single private ownership.
 }
 
 int main()
 {
-	unique_ptr<int> uniquePtr = make_unique<int>(5);
-	cout << uniquePtr << endl; // 000001F9403F7540
-	cout << *uniquePtr << endl; // 5
-	// unique pointer very similar to raw pointer about usage
-	// its SMART pointer because it automatically delete or free the memory when it goes out of scope
-
-	unique_ptr<int> anotherUniquePtr = move(uniquePtr);
-	cout << *anotherUniquePtr << endl; // 5
-	// unique ptr doesn't allow copy, but allows move. the move() function method transfers the ownership of the raw pointer internally to the other one
+	while (true)
+	{
+		printInt(5);
+	}
 }
 
 
