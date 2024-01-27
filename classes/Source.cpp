834ea3300eb54ed88432a13a4a76653e88cc6c1d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-27 09:05:27 +0300

    why do i need to define a class?

--- classes/Source.cpp
+++ classes/Source.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	string studentOneName = "Anil";
+	int studentOneID = 23;
+	int studentOneYear = 2;
+
+	string studentTwoName = "Ocean";
+	int studentTwoID = 24;
+	int studentTwoYear = 2;
+	// Yukarida yazdigim kod tamamen amele isi
+	// Boyle yapmak yerine class tanimlayabilirim
 }
